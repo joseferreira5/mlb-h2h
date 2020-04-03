fca9de143837cf729684448d2db9bb51c556bca2
Jose Ferreira <jferreira590@gmail.com>
2020-04-02 22:54:36 -0400

    Fixing height issues for deployed app on mobile.

--- src/components/styles/Main.js
+++ src/components/styles/Main.js
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 const Main = styled.main`
   max-width: 900px;
   margin: 0 auto;
-  min-height: calc(100% - 5em);
-  height: calc(100% - 5em);
+  min-height: 100%;
+  height: 100%;
   padding: 1.2em 0;
 `;
 
