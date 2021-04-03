ef9e1073a2aaf8888f04b7df50582bf924df4e14
Wilson Collado <colladow@gmail.com>
2021-04-02 22:24:14 -0400

    Adding files missed in last commit.

---
+++ src/utils/mapPlayer.ts
@@ -0,0 +1,17 @@
+import { Player } from '../types';
+
+export const mapBasicInfo = (data: Record<string, string>): Partial<Player> => ({
+  id: data.player_id,
+  proDebutDate: new Date(data.pro_debut_date),
+  bats: data.bats,
+  throws: data.throws,
+  position: data.position,
+});
+
+export const mapDetails = (data: Record<string, string>): Partial<Player> => ({
+  name: data.name_display_first_last_html,
+  jerseyNumber: data.jersey_number,
+  position: data.primary_position_txt,
+  age: Number(data.age),
+  height: `${data.height_feet}'${data.height_inches}"`,
+});
